faa35c36443c0519d66eaf3f4cd016b81e4e3ae7
Jeff DeLiso <jeff.deliso@gmail.com>
2019-02-12 12:57:25 -0500

    Change db file

--- lib/model/db_connection.rb
+++ lib/model/db_connection.rb
@@ -35,12 +35,12 @@ class DBConnection
 
   def self.execute(*args)
     print_query(*args)
-    instance.execute(*args)
+    instance.exec(*args)
   end
 
   def self.execute2(*args)
     print_query(*args)
-    instance.execute2(*args)
+    instance.exec(*args)
   end
 
   def self.last_insert_row_id
